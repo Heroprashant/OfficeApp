89dbbed969d92613fac966fad33ce091f3c85b65
Murat Aydin <Murat.Aydin@holland.ecsoft>
2016-06-10 15:56:22 +0200

    AddContact function works

--- .tmp/scripts/userprofile/userprofile.controller.js
+++ .tmp/scripts/userprofile/userprofile.controller.js
@@ -1,7 +1,15 @@
 /* global angular */
 /* global device */
 /* global navigator */
+/* global Contact */
 /* global ContactName */
+/* global ContactField */
+/* global ContactAddress */
+/* global ContactOrganization */
+/* global ContactFindOptions */
+/* global ContactError */
+/* global ContactFieldType */
+/* global cordova */
 (function () {
   'use strict';
   /**
@@ -32,6 +40,41 @@
 
     function activateUserprofile() {
 
+      userprofile.getBase64Image = function (imgElem) {
+        // imgElem must be on the same server otherwise a cross-origin error will be thrown "SECURITY_ERR: DOM Exception 18"
+        var canvas = document.createElement("canvas");
+        canvas.width = imgElem.clientWidth;
+        canvas.height = imgElem.clientHeight;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(imgElem, 0, 0);
+        var dataURL = canvas.toDataURL("image/png");
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+      };
+
+      userprofile.convertImgToDataURLviaCanvas = function (url, callback, outputFormat) {
+        var img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.onload = function () {
+          var canvas = document.createElement('CANVAS');
+          var ctx = canvas.getContext('2d');
+          var dataURL;
+          canvas.height = this.height;
+          canvas.width = this.width;
+          ctx.drawImage(this, 0, 0);
+          dataURL = canvas.toDataURL(outputFormat);
+          callback(dataURL);
+          canvas = null;
+        };
+        img.src = url;
+      };
+
+      //var imgData = JSON.stringify(userprofile.getBase64Image(document.getElementById('profilephoto')));
+
+
+      //console.log('Photo from profile ' + imgData);
+
+
+
       $ionicPlatform.ready(function () {
         if ($window.cordova) {
           userprofile.deviceinfo = device.platform;
@@ -48,32 +91,123 @@
         );
 
         userprofile.storeContact = function () {
-          if ($window.cordova) {
-            console.log( 'Running!' );
-            console.log( navigator.contacts );
 
+          if ($window.cordova) {
+            console.log('Running!');
+            //console.log(cordova.file);
+            //console.log(navigator.contacts);
 
             //{"fieldType":{"addresses":"addresses","birthday":"birthday","categories":"categories","country":"country","department":"department","displayName":"displayName","emails":"emails","familyName":"familyName","formatted":"formatted","givenName":"givenName","honorificPrefix":"honorificPrefix","honorificSuffix":"honorificSuffix","id":"id","ims":"ims","locality":"locality","middleName":"middleName","name":"name","nickname":"nickname","note":"note","organizations":"organizations","phoneNumbers":"phoneNumbers","photos":"photos","postalCode":"postalCode","region":"region","streetAddress":"streetAddress","title":"title","urls":"urls"}}
 
+            // create contact object
             var contact = navigator.contacts.create();
-            contact.displayName = 'Plumber2';
-            contact.nickname = 'Plumber2';
 
-            // populate some fields
-            var name = new ContactName();
-            name.givenName = 'John 2'; // Roepnaam
-            name.familyName = 'Doe'; // Achternaam
-            name.middleName = 'Van'; // Tussenvoegsel
-            contact.name = name;
+            //contact.id = '';
+            contact.displayName = userprofile.data.firstname + ' ' + userprofile.data.lastname;
 
-            var phoneNumbers = [];
-            phoneNumbers[0] = new ContactField('work', '768-555-1234', false);
-            phoneNumbers[1] = new ContactField('mobile', '999-555-5432', true); // preferred number
-            phoneNumbers[2] = new ContactField('home', '203-555-7890', false);
+            // contact -> name
+            var _name = new ContactName();
+            _name.givenName = userprofile.data.firstname; // Roepnaam
+            _name.familyName = userprofile.data.lastname; // Achternaam
+            contact.name = _name;
 
-            contact.phoneNumbers = phoneNumbers;
+            contact.nickname = '';
 
-            console.log( 'Created' );
+            // contact -> phonenumber(s)
+            var _phoneNumbers = [];
+            _phoneNumbers[0] = new ContactField(
+              'work',
+              userprofile.data.phoneNumber,
+              false
+            );
+            _phoneNumbers[1] = new ContactField(
+              'mobile',
+              userprofile.data.mobileNumber,
+              true // preferred number
+            );
+            contact.phoneNumbers = _phoneNumbers;
+
+            // contact -> email
+            var _emails = [];
+            _emails[0] = new ContactField(
+              'email',
+              userprofile.data.email,
+              false
+            );
+            contact.emails = _emails;
+
+            // contact -> address
+            var _adresses = [];
+            _adresses[0] = new ContactAddress(
+              '', // adress.pref
+              '', // adress.type
+              '', // adress.formatted
+              '', // adress.streetAddress
+              '', // adress.locality
+              '', // adress.region
+              '', // adress.postalCode
+              ''  // adress.country
+            );
+            contact.addresses = _adresses;
+
+            // contact -> ims
+            var _ims = [];
+            _ims[0] = new ContactField(
+              'ims',
+              '',
+              false
+            );
+            contact.ims = _ims;
+
+            // contact -> organization 
+            var organization = [];
+            organization[0] = new ContactOrganization(
+              '', // adress.pref
+              '', // adress.type
+              userprofile.data.company, // adress.name
+              userprofile.data.department, // adress.department
+              userprofile.data.unit + ' - ' + userprofile.data.team // adress.title
+            );
+            contact.organizations = organization;
+
+            var _birthDay = new Date();
+            contact.birthday = _birthDay.getDate();
+
+            contact.note = 'Ciber contact';
+
+            // contact -> photos
+            var imgData = '';
+            userprofile.convertImgToDataURLviaCanvas(document.getElementById('profilephoto').src, function (base64Img) {
+              imgData = base64Img;
+              console.log(imgData);
+            });
+            var _photos = [];
+            _photos[0] = new ContactField(
+              'url',
+              imgData,
+              false
+            );
+            contact.photos = _photos;
+
+            // contact -> categories
+            // var _categories = [];
+            // _categories[0] = new ContactField(
+            //   'ims',
+            //   '',
+            //   false
+            // );
+            // contact.categories = _categories;
+
+            // contact -> urls
+            // var _urls = [];
+            // _urls[0] = new ContactField(
+            //   'ims',
+            //   '',
+            //   false
+            // );
+            // contact.urls = _urls;
+
+            // save contact object
             contact.save(
               function (success) {
                 console.log('Success: ', success);
