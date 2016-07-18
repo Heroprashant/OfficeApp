83ecc95be1d7043750fea041b64c866b807bea11
Murat.Aydin <Murat.Aydin>
2016-07-18 15:49:13 +0200

    Information is changed from a slide page from left to a modal from bottom. The solution with 'page-slide-from-left' was not really excided so we decided to change it to a modal view

--- .tmp/scripts/core/layout/layout.controller.js
+++ .tmp/scripts/core/layout/layout.controller.js
@@ -1,66 +1,89 @@
 /* global angular */
-(function () {
-  'use strict';
-  /**
-  * @ngdoc controller
-  * @name core.layout.controller
-  * @requires Pocapp
-  * @requires core
-  * @requires core.layout
-  * @description
-  * <h1>Core Layout Controller</h1>
-  * <p>The Core Layout Controller allows you to control the functionalities for the page
-  where the Parent Nav view exists.</p>
-  * <p>Comes in handy when f.e. you would like to, for example, include a sidemenu</p>
-  */
-  angular
-    .module('Pocapp.core.layout')
-    .controller('LayoutCtrl', LayoutCtrl);
+(function() {
+    'use strict';
+    /**
+    * @ngdoc controller
+    * @name core.layout.controller
+    * @requires Pocapp
+    * @requires core
+    * @requires core.layout
+    * @description
+    * <h1>Core Layout Controller</h1>
+    * <p>The Core Layout Controller allows you to control the functionalities for the page
+    where the Parent Nav view exists.</p>
+    * <p>Comes in handy when f.e. you would like to, for example, include a sidemenu</p>
+    */
+    angular
+        .module('Pocapp.core.layout')
+        .controller('LayoutCtrl', LayoutCtrl);
 
-  LayoutCtrl.$inject = ['$ionicViewSwitcher', '$state', '$ionicHistory'];
+    LayoutCtrl.$inject = ['$scope', '$ionicViewSwitcher', '$state', '$ionicHistory', '$ionicModal'];
 
-  function LayoutCtrl($ionicViewSwitcher, $state, $ionicHistory) {
-    var layout = this;
-    layout.showInformationIcon = true;
-    layout.showGoBackButton = true;
+    function LayoutCtrl($scope, $ionicViewSwitcher, $state, $ionicHistory, $ionicModal) {
+        var layout = this;
+        layout.showInformationIcon = true;
+        layout.showGoBackButton = true;
 
-    activate();
+        activate();
 
-    function activate() {
+        function activate() {
 
-      layout.showInformation = function() {
-        $ionicViewSwitcher.nextDirection('back');
-        $state.go('Pocapp.information');
-        setInformationIconOnHeaderBar(false);
-        setGoBackButton(false);
-      };
+            layout.showInformation = function() {
+                // $ionicViewSwitcher.nextDirection('back');
+                // $state.go('Pocapp.information');
+                // setInformationIconOnHeaderBar(false);
+                // setGoBackButton(false);
+            };
 
-      layout.closeInformation = function() {
-        $ionicViewSwitcher.nextDirection('forward');
-        $ionicHistory.goBack();
-        setInformationIconOnHeaderBar(true);
-        setGoBackButton(true);
-      };
+            // layout.closeInformation = function() {
+            //   $ionicViewSwitcher.nextDirection('forward');
+            //   $ionicHistory.goBack();
+            //   setInformationIconOnHeaderBar(true);
+            //   setGoBackButton(true);
+            // };
 
-      function setInformationIconOnHeaderBar(show) {
-        if(show){
-          layout.showInformationIcon = true;
+            function setInformationIconOnHeaderBar(show) {
+                if (show) {
+                    layout.showInformationIcon = true;
+                } else {
+                    layout.showInformationIcon = false;
+                }
+            }
+
+            function setGoBackButton(show) {
+                if (show) {
+                    layout.showGoBackButton = true;
+                } else {
+                    layout.showGoBackButton = false;
+                }
+            }
+
+            $ionicModal.fromTemplateUrl('my-modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function(modal) {
+                $scope.modal = modal;
+            });
+            layout.openModal = function() {
+                $scope.modal.show();
+            };
+            layout.closeModal = function() {
+                $scope.modal.hide();
+            };
+            // Cleanup the modal when we're done with it!
+            $scope.$on('$destroy', function() {
+                $scope.modal.remove();
+            });
+            // Execute action on hide modal
+            $scope.$on('modal.hidden', function() {
+                // Execute action
+            });
+            // Execute action on remove modal
+            $scope.$on('modal.removed', function() {
+                // Execute action
+            });
+
+            return layout;
         }
-        else {
-          layout.showInformationIcon = false;
-        }
-      }
-
-      function setGoBackButton(show) {
-        if(show){
-          layout.showGoBackButton = true;
-        }
-        else {
-          layout.showGoBackButton = false;
-        }
-      }
-
-      return layout;
     }
-  }
 })();
